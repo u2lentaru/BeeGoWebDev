65e61c9d54ddef22387372ab0fb8b8df8064f1e7
u2 <u2@lenta.ru>
2020-03-19 15:46:16 +0500

    database tests done

--- testdb/db_func.go
+++ testdb/db_func.go
@@ -63,6 +63,7 @@ func (e TExplorer) Truncate() error {
 	_, err := c.DeleteMany(context.Background(), bson.D{})
 
 	return err
+
 }
 
 //func getBlog(db *sql.DB, id string) (models.TBlog, error) {
@@ -81,4 +82,61 @@ func (e TExplorer) getBlog() ([]models.TPost, error) {
 	}
 
 	return blog, nil
+
+}
+
+func (e TExplorer) getPost(id string) (models.TPost, error) {
+	c := e.Db.Database(e.DbName).Collection(e.DbCollection)
+
+	filter := bson.D{{Key: "id", Value: id}}
+
+	res := c.FindOne(context.Background(), filter)
+
+	post := new(models.TPost)
+	if err := res.Decode(post); err != nil {
+		return models.TPost{}, errors.Wrap(err, "decode")
+	}
+
+	return *post, nil
+
+}
+
+func (e TExplorer) editPost(post *models.TPost, id string) error {
+	filter := bson.D{{Key: "id", Value: id}}
+
+	update := createUpdates(*post)
+
+	c := e.Db.Database(e.DbName).Collection(e.DbCollection)
+	_, err := c.UpdateOne(context.Background(), filter, update)
+
+	return err
+
+}
+
+func createUpdates(post models.TPost) bson.D {
+	update := bson.D{}
+	if len(post.Subj) != 0 {
+		update = append(update, bson.E{Key: "Subj", Value: post.Subj})
+	}
+
+	if len(post.PostTime) != 0 {
+		update = append(update, bson.E{Key: "PostTime", Value: post.PostTime})
+	}
+
+	if len(post.PostText) != 0 {
+		update = append(update, bson.E{Key: "PostText", Value: post.PostText})
+	}
+
+	return bson.D{{Key: "$set", Value: update}}
+
+}
+
+func (e TExplorer) deletePost(id string) error {
+	filter := bson.D{{Key: "id", Value: id}}
+
+	c := e.Db.Database(e.DbName).Collection(e.DbCollection)
+	_, err := c.DeleteOne(context.Background(), filter)
+
+	return err
+
 }
--- testdb/db_test.go
+++ testdb/db_test.go
@@ -11,21 +11,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// TExplorer - Explorer for tests
-/*type TExplorer struct {
-	Db           *mongo.Client
-	DbName       string
-	DbCollection string
-}*/
-
-// TestaddPost add post
-func TestAddPost(t *testing.T) {
-	/*db, err := mongo.NewClient(options.Client().ApplyURI("mongodb://192.168.0.103:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	*/
-
+// TestDb test database functions
+func TestDb(t *testing.T) {
 	e, err := initDb()
 	if err != nil {
 		t.Error(err)
@@ -50,14 +37,54 @@ func TestAddPost(t *testing.T) {
 	}
 
 	resultPosts, err := e.getBlog()
-
-	if err := e.addPost(post); err != nil {
+	if err != nil {
 		t.Error(err)
 	}
 
 	newPosts := append(initialState, post)
-	//if !reflect.DeepEqual(resultPosts, initialState) {
 	if !reflect.DeepEqual(resultPosts, newPosts) {
+		t.Errorf("Expected %v, result %v", newPosts, resultPosts)
+	}
+
+	resultPost, err := e.getPost(post.ID)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(post, resultPost) {
+		t.Errorf("Expected %v, result %v", post, resultPost)
+	}
+
+	post = models.TPost{
+		ID:       "100",
+		Subj:     "NewUpdPostSubj",
+		PostTime: "2020-03-04",
+		PostText: "NewUpdPostText",
+	}
+
+	if err := e.editPost(&post, post.ID); err != nil {
+		t.Error(err)
+	}
+
+	resultPosts, err = e.getBlog()
+	if err != nil {
+		t.Error(err)
+	}
+
+	newPosts = append(initialState, post)
+	if !reflect.DeepEqual(resultPosts, newPosts) {
+		t.Errorf("Expected %v, result %v", newPosts, resultPosts)
+	}
+
+	if err := e.deletePost(post.ID); err != nil {
+		t.Error(err)
+	}
+
+	resultPosts, err = e.getBlog()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(resultPosts, initialState) {
 		t.Errorf("Expected %v, result %v", initialState, resultPosts)
 	}
 }
@@ -79,11 +106,10 @@ func initDb() (TExplorer, error) {
 		DbCollection: "posts",
 	}
 
-	log.Printf("e %v", e)
-
 	if err := e.InsertDefault(); err != nil {
 		return TExplorer{}, err
 	}
 
 	return e, nil
+
 }
